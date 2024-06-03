c50f11974e1e7f8efc9276c2ed0e7e6d60507e23
Clawiatura <maksimov19992805@icloud.com>
2024-06-03 12:20:06 +0200

    Добавьте файлы проекта.

---
+++ Sort 33.3/Program.cs
@@ -0,0 +1,25 @@
+﻿int sort = 0;
+int Even;
+int Odd;
+
+for (int i = 1; i <= 100; i++)
+{
+
+
+    sort = i;
+   
+    if (sort % 2 == 0)
+    {
+        Even = i;
+        Console.Write(Even + " ");
+    }
+}
+  for (int i = 1;i<= 100; i+=2)
+{
+    Odd = i;
+    Console.Write(Odd+" ");
+
+}
+
+
+
